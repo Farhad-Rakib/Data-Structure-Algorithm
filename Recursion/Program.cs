32e74419f70fb089959ac3c207847b5675413204
Farhad Rakib <rakib.farhad@surbanajurong.com>
2023-12-27 16:03:51 +0600

    Summaction of Array using recursion

---
+++ Recursion/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Recursion
+{
+    public class Program
+    {
+        static int sum = 0;
+        static void Main(string[] args)
+        {
+            int[] array = { 2,4,6 };
+            Console.WriteLine (sumOfArrayusingRecursion(array));
+        }
+
+        static int sumOfArrayusingRecursion(int[] array)
+        {
+            if (array == null && array.Length == 0) 
+            { 
+                return 0; 
+            }
+            
+            if (array.Length > 0) 
+            { 
+                
+                List<int> list = new List<int>(array);
+                list.RemoveAt(0);
+                sumOfArrayusingRecursion(list.ToArray());
+                sum += array[0];
+
+            }
+
+            return sum;
+        }
+    }
+}
